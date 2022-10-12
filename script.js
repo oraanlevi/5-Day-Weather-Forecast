b636355fa974d5c5825ff1a38237bb8083e40eef
Oraan Levi <oraanlevi1@gmail.com>
2022-10-12 16:08:17 -0700

    adding css

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const form = document.querySelector("form");
 const input = document.querySelector("form input");
 const msg = document.querySelector("#msg");
 
+// const response = 
+
 // $(document).ready(function () {
 //   var searcnBtn = $("#searchBtn");
 
@@ -60,6 +62,7 @@ form.addEventListener("submit", event => {
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${weatherApiKey}&units=metric`;
 
+
   // const forecastUrl = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`
   // fetch(forecastUrl)
   // .then(response => response.json())
@@ -70,23 +73,47 @@ form.addEventListener("submit", event => {
   //   msg.textContent = "Please search for a valid city 😩";
   // });
   console.log(inputVal)
-  console.log(event)
+  // console.log(event)
   fetch(url)
       .then(response => response.json())
       .then(data => {
         console.log(data)
         msg.textContent = data.name
-       lat =  data.coord.lat
-       lon =  data.coord.lon
+        lat =  data.coord.lat
+        lon =  data.coord.lon
+        console.log(lat, lon);
+        displayHumidity(data);
       })
-      .catch(() => {
-        msg.textContent = "Please search for a valid city 😩";
-      });
+//      .catch(() => {
+//        msg.textContent = "Please search for a valid city 😩";
+//      });
+
 })
+
+function displayHumidity(data) {
+  console.log(data);
+  const humidity = data.main.humidity;
+  const temp = data.main.temp;
+  const wind = data.wind.deg;
+  const humid = data.main.humidity;
+  const currentHumid = document.querySelector('#currentHumid');
+  const currentTemp = document.querySelector('#currentTemp');
+  const currentWind = document.querySelector('#currentWind');
+  console.log(currentTemp)
+  currentTemp.innerHTML = temp
+  currentWind.innerHTML = wind
+  currentHumid.innerHTML = humid
+
+  console.log(humidity)
+
+
+}
+
+
 
 // render currwent weather
 
 // render forecast
 // looping through weather data, inside that foor loop, you will attach that data to the card
 
-// create a card that shows the forecast 
+// create a card that shows the forecast
